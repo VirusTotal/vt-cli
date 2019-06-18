4c32c88b17258b6192c3cfeb7251d74a2df36b0a
Victor M. Alvarez <vmalvarez@virustotal.com>
2019-06-18 20:06:31 +0200

    Fix misleading error message.

--- cmd/retrohunt.go
+++ cmd/retrohunt.go
@@ -36,7 +36,7 @@ func retrohuntListTable(cmd *cobra.Command) error {
 
 	for _, flag := range []string{"cursor", "include", "exclude"} {
 		if cmd.Flag(flag).Changed {
-			return fmt.Errorf("--%s must be used with --yaml", flag)
+			return fmt.Errorf("--%s can't be used with --table", flag)
 		}
 	}
 
