28e249b671addbdba9b32a948ad711884f0137b0
Jose Toro <josetoro@google.com>
2022-04-13 17:07:57 +0200

    feat(Collections): Remove items / delete collection (#46)

--- cmd/collection.go
+++ cmd/collection.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"net"
+	"regexp"
 	"strings"
 
 	"github.com/VirusTotal/vt-go"
@@ -11,6 +13,12 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+type objectDescriptor struct {
+	Type string `json:"type"`
+	Id   string `json:"id,omitempty"`
+	Url  string `json:"url,omitempty"`
+}
 
 var collectionCmdHelp = `Get information about one or more collections.
 
@@ -49,6 +57,8 @@ func NewCollectionCmd() *cobra.Command {
 	cmd.AddCommand(NewCollectionCreateCmd())
 	cmd.AddCommand(NewCollectionRenameCmd())
 	cmd.AddCommand(NewCollectionUpdateCmd())
+	cmd.AddCommand(NewCollectionDeleteCmd())
+	cmd.AddCommand(NewCollectionRemoveItemsCmd())
 
 	addRelationshipCmds(cmd, "collections", "collection", "[collection]")
 	addThreadsFlag(cmd.Flags())
@@ -200,6 +210,82 @@ func NewCollectionUpdateCmd() *cobra.Command {
 	}
 }
 
+var removeCollectionItemsCmdHelp = `Remove items from a collection.
+
+This command receives a collection ID and one of more IoCs
+(sha256 hashes, URLs, domains, IP addresses) and removes them from the collection.
+
+If the command receives a single hypen (-) the IoCs will be read from the
+standard input.`
+
+var removeCollectionItemsExample = `  vt collection remove [collection id] www.example.com
+  vt collection remove [collection id] www.example.com 8.8.8.8
+  cat list_of_iocs | vt collection remove [collection id] -`
+
+// NewCollectionRemoveItemsCmd returns a command for removing items from a collection.
+func NewCollectionRemoveItemsCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "remove [collection id] [ioc]...",
+		Short:   "Remove items from a collection.",
+		Args:    cobra.MinimumNArgs(2),
+		Long:    removeCollectionItemsCmdHelp,
+		Example: removeCollectionItemsExample,
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			c, err := NewAPIClient()
+			if err != nil {
+				return err
+			}
+			relationshipDescriptors := descriptorsFromReader(
+				utils.StringReaderFromCmdArgs(args[1:]))
+			for relationshipName, descriptors := range relationshipDescriptors {
+				url := vt.URL("collections/%s/%s", args[0], relationshipName)
+				response, err := c.DeleteData(url, descriptors)
+				if err != nil {
+					return err
+				}
+				if response.Error.Code != "" {
+					return response.Error
+				}
+			}
+			return nil
+		},
+	}
+}
+
+var deleteCollectionCmdHelp = `Delete a collection.
+
+This command receives a collection ID and deletes it.`
+
+var deleteCollectionExample = `  vt collection delete [collection id]`
+
+// NewCollectionDeleteCmd returns a command for deleting a collection.
+func NewCollectionDeleteCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "delete [collection id]",
+		Short:   "Delete a collection.",
+		Args:    cobra.MinimumNArgs(1),
+		Long:    deleteCollectionCmdHelp,
+		Example: deleteCollectionExample,
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			c, err := NewAPIClient()
+			if err != nil {
+				return err
+			}
+			url := vt.URL("collections/%s", args[0])
+			response, err := c.Delete(url)
+			if err != nil {
+				return err
+			}
+			if response.Error.Code != "" {
+				return response.Error
+			}
+			return nil
+		},
+	}
+}
+
 func rawFromReader(reader utils.StringReader) string {
 	var lines []string
 	for {
@@ -210,4 +296,52 @@ func rawFromReader(reader utils.StringReader) string {
 		lines = append(lines, next)
 	}
 	return strings.Join(lines, " ")
+}
+
+func descriptorsFromReader(reader utils.StringReader) map[string][]objectDescriptor {
+	descriptors := make(map[string][]objectDescriptor)
+	hashPattern := regexp.MustCompile("[0-9a-fA-F]{32,64}")
+	urlPattern := regexp.MustCompile("[hH][tTxX]{2}[pP][sS]?://.*")
+	// At least two domain parts.
+	domainPattern := regexp.MustCompile(".*[^.]+\\.[^.]+.*")
+	for {
+		next, err := reader.ReadString()
+		if err == io.EOF {
+			break
+		}
+		if match := hashPattern.MatchString(next); match {
+			files := descriptors["files"]
+			files = append(files, objectDescriptor{
+				Type: "file",
+				Id:   next,
+			})
+			descriptors["files"] = files
+		} else if net.ParseIP(next) != nil {
+			if strings.Contains(next, ".") {
+				ipAddresses := descriptors["ip_addresses"]
+				ipAddresses = append(ipAddresses, objectDescriptor{
+					Type: "ip_address",
+					Id:   next,
+				})
+				descriptors["ip_addresses"] = ipAddresses
+			} else {
+				// IPv6, skip.
+			}
+		} else if urlPattern.MatchString(next) {
+			urls := descriptors["urls"]
+			urls = append(urls, objectDescriptor{
+				Type: "url",
+				Url:  next,
+			})
+			descriptors["urls"] = urls
+		} else if domainPattern.MatchString(next) {
+			domains := descriptors["domains"]
+			domains = append(domains, objectDescriptor{
+				Type: "domain",
+				Id:   next,
+			})
+			descriptors["domains"] = domains
+		}
+	}
+	return descriptors
 }
