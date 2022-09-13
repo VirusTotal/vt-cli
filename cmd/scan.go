932cdbf7a3b5ce157d38a336f09ca839ca64d758
Marta Gómez Macías <mgmacias@google.com>
2022-09-13 10:00:02 +0200

    Improve documentation

--- cmd/scan.go
+++ cmd/scan.go
@@ -64,16 +64,19 @@ You can use the "vt analysis" command for retrieving information about the
 analyses.
 
 If the command receives a single hypen (-) the file paths are read from the standard
-input, one per line.`
+input, one per line.
+
+The command can also receive a directory to scan all files contained on it.`
 
 var scanFileCmdExample = `  vt scan file foo.exe
   vt scan file foo.exe bar.exe
+	vt scan file foo/
   cat list_of_file_paths | vt scan file -`
 
 // NewScanFileCmd returns a new instance of the 'scan file' command.
 func NewScanFileCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "file [file]...",
+		Use:     "file [[dir] | [file]...]",
 		Short:   "Scan one or more files",
 		Long:    scanFileCmdHelp,
 		Example: scanFileCmdExample,
